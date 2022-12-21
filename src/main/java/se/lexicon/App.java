e0744f1342203d10e51a879d659bb9dde04e4956
MariusAdrianStoica <stoichitza@yahoo.com>
2022-12-22 00:59:55 +0100

    City findById(int id) List<City> findByCode(String code) List<City> findByName(String name) LIst<City> findAll()

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,50 +1,50 @@
 package se.lexicon;
 
+import se.lexicon.dao.CityDaoJDBC;
 import se.lexicon.exception.DBConnectionException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class App{
-public static void main( String[] args ) {
+public class App {
+  public static void main(String[] args) {
 
-  System.out.println( "JDBC Workshop");
-  System.out.println("-------------------------------");
+    System.out.println("JDBC Workshop");
+    System.out.println("-------------------------------");
 
-  String query = "SELECT * FROM CITY WHERE ID =?";
-  try(
-          Connection connection = MySQLConnection.getConnection();
-          PreparedStatement preparedStatement = connection.prepareStatement(query);
-          ){
+        CityDaoJDBC cityDaoJDBC = new CityDaoJDBC();
 
-          preparedStatement.setInt(1,99 );
-          try(ResultSet resultSet = preparedStatement.executeQuery()){
-
-            while (resultSet.next()){
-
-              System.out.print(resultSet.getInt("ID") + "\t");
-              System.out.print(resultSet.getString("Name") + "\t");
-              System.out.print(resultSet.getString("CountryCode") + "\t");
-              System.out.print(resultSet.getString("District") + "\t");
-              System.out.print(resultSet.getInt("Population") + "\n");
-              System.out.println("------------------");
-            }
-          }
-
-
-  }catch(DBConnectionException | SQLException e){
-    System.out.println(e.getMessage());
-    e.printStackTrace();
-
-  }
+        //cityDaoJDBC.findByID(34);
+        //cityDaoJDBC.findByCode("ROM");
+        //cityDaoJDBC.findByName("Ro%");
+        cityDaoJDBC.findAll();
 
 
 
 
 
-  System.out.println("Code by : Marius Stoica");
-  System.out.println("Have a good day!");
+
+
+        //System.out.println("-------------------------------");
+        System.out.println("\nCode by : Marius Stoica");
+        System.out.println("Have a good day!");
+      }
     }
-}
+
+
+
+    /*public static int getInputNumber () {
+      Scanner scanner = new Scanner(System.in);
+      int number = 0;
+      try {
+        number = scanner.nextInt();
+      } catch (InputMismatchException e) {
+        System.out.println("\t\t Enter a valid number between 0-6");
+      }
+
+      return number;
+    }*/
--- src/main/java/se/lexicon/dao/CityDaoJDBC.java
+++ src/main/java/se/lexicon/dao/CityDaoJDBC.java
@@ -1,7 +1,13 @@
 package se.lexicon.dao;
 
+import se.lexicon.MySQLConnection;
+import se.lexicon.exception.DBConnectionException;
 import se.lexicon.model.City;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,28 +15,172 @@ public class CityDaoJDBC implements CityDao{
 
     private List<City> cityStorage;
 
+    private static String query;
+
+
     public CityDaoJDBC(){
-        cityStorage=new ArrayList<>();
+
+
     }
 
     @Override
-    public City findByID(int id) {
-        return null;
-    }
+    public  City findByID(int id) {
 
+        System.out.println("Find City By ID: "+id);
+        System.out.println("******************************");
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id must be a positive number!");
+        }
+            query = "SELECT * FROM CITY WHERE ID = ?";
+
+            try (
+                    Connection connection = MySQLConnection.getConnection();
+                    PreparedStatement preparedStatement = connection.prepareStatement(query)
+            ) {
+
+                preparedStatement.setInt(1, id);
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                    while (resultSet.next()) {
+
+                        System.out.println("Id\t\t\t:" + resultSet.getInt("ID"));
+                        System.out.println("Name \t\t:" + resultSet.getString("Name"));
+                        System.out.println("CountryCode :" + resultSet.getString("CountryCode"));
+                        System.out.println("District \t:" + resultSet.getString("District"));
+                        System.out.println("Population  :" + resultSet.getInt("Population"));
+                        System.out.println("-------------------------------");
+                    }
+                }
+
+            } catch (DBConnectionException | SQLException e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+
+            }
+
+            return null;
+
+        }
     @Override
     public List<City> findByCode(String code) {
+        System.out.println("Find Cities By CountryCode: "+code);
+        System.out.println("******************************");
+
+        query = "SELECT * FROM CITY WHERE CountryCode = ?";
+        cityStorage = new ArrayList<>();
+
+        try (
+                Connection connection = MySQLConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)
+        ) {
+
+            preparedStatement.setString(1, code);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                while (resultSet.next()) {
+
+                    cityStorage.add(
+                            new City(
+                                    resultSet.getInt("ID"),
+                                    resultSet.getString("Name"),
+                                    resultSet.getString("CountryCode"),
+                                    resultSet.getString("District"),
+                                    resultSet.getInt("Population")
+                                ));
+                }
+
+            }
+
+        } catch (DBConnectionException | SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("There are " + cityStorage.size() +" cities in the table, with CountryCode " + code);
+        cityStorage.forEach(System.out::println);
+
+        System.out.println("-------------------------------");
         return null;
     }
 
     @Override
     public List<City> findByName(String name) {
+        System.out.println("Find Cities By Name: "+name);
+        System.out.println("******************************");
+
+        query = "SELECT * FROM CITY WHERE Name like ?";
+        cityStorage = new ArrayList<>();
+
+        try (
+                Connection connection = MySQLConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)
+        ) {
+
+            preparedStatement.setString(1, name);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                while (resultSet.next()) {
+                    cityStorage.add(
+                            new City(
+                                    resultSet.getInt("ID"),
+                                    resultSet.getString("Name"),
+                                    resultSet.getString("CountryCode"),
+                                    resultSet.getString("District"),
+                                    resultSet.getInt("Population")
+                            ));
+                }
+
+            }
+
+        } catch (DBConnectionException | SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("There are " + cityStorage.size() +" cities in the table, with Name: " + name);
+        cityStorage.forEach(System.out::println);
+        System.out.println("-------------------------------");
         return null;
     }
 
     @Override
     public List<City> findAll() {
-        return new ArrayList<>(cityStorage);
+        System.out.println("Find all cities: ");
+        System.out.println("******************************");
+
+        query = "SELECT * FROM CITY";
+        cityStorage = new ArrayList<>();
+
+        try (
+                Connection connection = MySQLConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(query)
+        ) {
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                while (resultSet.next()) {
+                    cityStorage.add(
+                            new City(
+                                    resultSet.getInt("ID"),
+                                    resultSet.getString("Name"),
+                                    resultSet.getString("CountryCode"),
+                                    resultSet.getString("District"),
+                                    resultSet.getInt("Population")
+                            ));
+                }
+
+            }
+
+        } catch (DBConnectionException | SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("There are " + cityStorage.size() +" cities in the table:");
+        cityStorage.forEach(System.out::println);
+        System.out.println("-------------------------------");
+
+        return null;
     }
 
     @Override
